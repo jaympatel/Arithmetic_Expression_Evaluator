3de02337d40f70c444582cd66ce41949d9c170ef
Jay <jaympatel567@gmail.com>
2015-04-27 10:54:23 -0400

    change

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import ExpressionEvaluation.views
 
 urlpatterns = patterns('',
     # Examples:
