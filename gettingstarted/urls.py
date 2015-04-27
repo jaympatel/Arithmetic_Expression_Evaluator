82f58d50f02a2ff0d28bbcc2b54424fe97fb0176
Jay <jaympatel567@gmail.com>
2015-04-27 10:59:07 -0400

    change

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
 
     #url(r'^$', hello.views.index, name='index'),
     url(r'^$', ExpressionEvaluation.views.get_expression, name='get_expression'),
-    url(r'^db', ExpressionEvaluation.views.db, name='db'),
+    #url(r'^db', ExpressionEvaluation.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
